58607865d50e9acc02de8bc2febcb75521ce0cb2
Marta <marta.verweij@cern.ch>
2015-11-17 23:58:21 +0100

    mc tune to data files

---
+++ HFVoronoiCalibFiles/Hydjet_Quenched_MinBias_5020GeV_750/TuneMCtwangToData/make_db_s8NhYJ518x.py
@@ -0,0 +1,57 @@
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+ivars = VarParsing.VarParsing('standard')
+
+ivars.register ('outputTag',
+                mult=ivars.multiplicity.singleton,
+                mytype=ivars.varType.string,
+                info="for testing")
+#ivars.outputTag="UETable_PF_v00_mc"
+ivars.outputTag="UETable_PF_HYDJET_5020GeV_754_38T_v00_data"
+
+ivars.register ('inputFile',
+                mult=ivars.multiplicity.singleton,
+                mytype=ivars.varType.string,
+                info="for testing")
+
+ivars.register ('outputFile',
+                mult=ivars.multiplicity.singleton,
+                mytype=ivars.varType.string,
+                info="for testing")
+
+ivars.inputFile="ue_calibrations_pf_mc.txt"
+#ivars.outputFile="./output.db"
+ivars.outputFile="./UETable_PF_HYDJET_5020GeV_754_38T_v00_data.db"
+
+ivars.parseArguments()
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('DUMMY')
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.source = cms.Source("EmptyIOVSource",
+                            timetype = cms.string("runnumber"),
+                            firstValue = cms.uint64(1),
+                            lastValue = cms.uint64(1),
+                            interval = cms.uint64(1)
+                            )
+
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.CondDBCommon.connect = "sqlite_file:" + ivars.outputFile
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+                                          process.CondDBCommon,
+                                          timetype = cms.untracked.string("runnumber"),
+                                          toPut = cms.VPSet(cms.PSet(record = cms.string('JetCorrectionsRecord'),
+                                                                     tag = cms.string(ivars.outputTag)
+                                                                     )
+                                                            )
+                                          )
+
+process.makeUETableDB = cms.EDAnalyzer('UETableProducer',
+                                       txtFile = cms.string(ivars.inputFile),
+				       jetCorrectorFormat = cms.untracked.bool(True)
+                                       )
+
+process.step  = cms.Path(process.makeUETableDB)
